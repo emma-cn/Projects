36786a9aebe9343b6a34c424ceaf19faee7f2536
emma-cn <emma_cn@qq.com>
2024-08-10 11:28:02 +0800

    torch

---
+++ Python/pytorch-learn.py
@@ -0,0 +1,25 @@
+# https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Download training data from open datasets.
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+
